5a708c41fd48fe33f84f0143e1d856bbbd12e1a5
Kevin Breinert <98883117+kevinbreinert@users.noreply.github.com>
2023-08-30 20:26:33 +0200

    first commit

---
+++ speiseplan/speiseplanApp.swift
@@ -0,0 +1,17 @@
+//
+//  speiseplanApp.swift
+//  speiseplan
+//
+//  Created by Kevin Breinert on 30.08.23.
+//
+
+import SwiftUI
+
+@main
+struct speiseplanApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
